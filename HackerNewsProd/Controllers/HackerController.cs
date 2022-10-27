775f939f4c8e1df2c0d1b94a301c218db2a7c4e1
Dhruvpatelcode <dhruvpatelcode@gmail.com>
2022-10-27 14:57:25 -0400

    Added methods to eliminate any nulls coming in.

--- HackerNewsProd/Controllers/HackerController.cs
+++ HackerNewsProd/Controllers/HackerController.cs
@@ -2,6 +2,7 @@
 using HackerNewsProd.Services;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
